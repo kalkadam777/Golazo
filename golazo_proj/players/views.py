ceff099f2cd1d3061c0a6c501ed40dc49ecd2d3d
Reshord <daribayev1982@bk.ru>
2024-10-19 20:04:26 +0500

    filter

--- golazo_proj/players/views.py
+++ golazo_proj/players/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Player
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 def home(request):
     return render(request, 'home.html')
@@ -9,8 +10,34 @@ class PlayerListView(ListView):
     model = Player
     template_name = 'players/player_list.html'
     context_object_name = 'players'
-    ordering = ['name']
-    paginate_by = 10 
+    paginate_by = 20
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) |
+                Q(current_club__name__icontains=query)
+            )
+
+        position = self.request.GET.get('position')
+        if position and position != 'All':
+            queryset = queryset.filter(position=position)
+
+        min_value = self.request.GET.get('min_value')
+        max_value = self.request.GET.get('max_value')
+        if min_value and max_value:
+            queryset = queryset.filter(value__gte=min_value, value__lte=max_value)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['positions'] = Player.POSITION_CHOICES  
+        context['min_value'] = self.request.GET.get('min_value', 0)
+        context['max_value'] = self.request.GET.get('max_value', 100000000)  
+        return context
 
 class PlayerDetailView(DetailView):
     model = Player
