3c84558c38447a2eaf4399ad92039ff802f39611
Abukhanifa <a_kuatuly@kbtu.kz>
2024-12-10 11:10:59 +0500

    changed serializers

--- golazo_proj/clubs/serializers.py
+++ golazo_proj/clubs/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Club
+from players.models import Player
+
+
+class PlayerNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ['id', 'name', 'position', 'value', 'goals', 'assists', 'age', 'photo']  # Include only essential fields
+
 
 class ClubSerializer(serializers.ModelSerializer):
+    players = PlayerNestedSerializer(many=True, read_only=True)  # Use the lightweight serializer
+
     class Meta:
         model = Club
         fields = '__all__'
--- golazo_proj/players/serializers.py
+++ golazo_proj/players/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 from .models import Player, TransferHistory
-from clubs.serializers import ClubSerializer
+from clubs.models import Club
+
+class ClubNestedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ['id', 'name', 'value', 'country', 'emblem', 'league']  # Include only essential fields
+
 
 class PlayerSerializer(serializers.ModelSerializer):
-    current_club = ClubSerializer()
+    current_club = ClubNestedSerializer(read_only=True)  # Use the lightweight serializer
+
     class Meta:
         model = Player
         fields = ['id', 'name', 'position', 'value', 'country', 'goals', 'assists', 'age', 'photo', 'current_club']
+
 
 class TransferHistorySerializer(serializers.ModelSerializer):
     player = serializers.StringRelatedField()
     club = serializers.StringRelatedField()
     
-
     class Meta:
         model = TransferHistory
         fields = '__all__'
