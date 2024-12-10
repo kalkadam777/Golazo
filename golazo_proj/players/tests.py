a6104c53d88e9a11aec1145b73576daac8728364
Reshord <daribayev1982@bk.ru>
2024-12-11 02:30:46 +0500

    tests for apps

--- golazo_proj/players/tests.py
+++ golazo_proj/players/tests.py
@@ -1,107 +1,90 @@
+from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from clubs.models import Club
-from .models import Player
-
+from .models import Player, Club
 
 class PlayerAPITestCase(APITestCase):
-    def setUp(self):
-
-        self.club = Club.objects.create(name="Test Club", value=5000000.00, country="US", league="Test League")
-
-
-        self.player1 = Player.objects.create(
-            name="Player 1",
-            position="Forward",
-            current_club=self.club,
-            value=1000000.00,
-            country="US",
-            goals=10,
-            assists=5,
-            age=25
-        )
-        self.player2 = Player.objects.create(
-            name="Player 2",
-            position="Midfielder",
-            current_club=self.club,
-            value=2000000.00,
+    @classmethod
+    def setUpTestData(cls):
+        cls.club = Club.objects.create(
+            name="Manchester United",
+            value=854000000,
             country="GB",
-            goals=20,
-            assists=10,
-            age=22
+            league="Premier League"
+        )
+        cls.player1 = Player.objects.create(
+            name="Harry Kane",
+            position="Forward",
+            value=100000000,
+            country="FI",
+            goals=5,
+            assists=6,
+            age=31,
+            current_club=cls.club
+        )
+        cls.player2 = Player.objects.create(
+            name="Harry Maguire",
+            position="Defender",
+            value=18000000,
+            country="GB",
+            goals=0,
+            assists=0,
+            age=31,
+            current_club=cls.club
         )
 
-        self.player_list_url = reverse('player-list')
-        self.player_detail_url = reverse('player-detail', kwargs={'pk': self.player1.id})
-        self.valuable_players_url = reverse('valuable-players-list')
-        self.young_players_url = reverse('young-player-list')
-        self.player_create_url = reverse('player-create')
-        self.player_update_url = reverse('player-update', kwargs={'pk': self.player1.id})
-        self.player_delete_url = reverse('player-delete', kwargs={'pk': self.player1.id})
-
-    def test_player_list(self):
-
-        response = self.client.get(self.player_list_url)
+    def test_list_players(self):
+        url = reverse('player-list')
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+        self.assertIn('results', response.data)
+        self.assertEqual(len(response.data['results']), 2)  
 
-    def test_player_detail(self):
-
-        response = self.client.get(self.player_detail_url)
+    def test_search_players(self):
+        url = reverse('player-list') + '?search=Harry'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], self.player1.name)
+        self.assertEqual(len(response.data['results']), 2) 
 
-    def test_valuable_players(self):
-
-        response = self.client.get(self.valuable_players_url)
+    def test_filter_by_position(self):
+        url = reverse('player-list') + '?position=Forward'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-        self.assertEqual(response.data[0]['name'], "Player 2")
+        self.assertEqual(len(response.data['results']), 1)  
 
-    def test_young_players(self):
-
-        response = self.client.get(self.young_players_url)
+    def test_filter_by_value_range(self):
+        url = reverse('player-list') + '?value__gte=50000000&value__lte=150000000'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
-        self.assertEqual(response.data[0]['name'], "Player 2")
+        self.assertEqual(len(response.data['results']), 1) 
+
+    def test_ordering_players(self):
+        url = reverse('player-list') + '?ordering=age'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'][0]['name'], "Harry Kane")
 
     def test_create_player(self):
-
+        url = reverse('player-create')
         data = {
-            "name": "Player 3",
-            "position": "Defender",
-            "current_club": self.club.id,
-            "value": "1500000.00",
-            "country": "FR",
-            "goals": 5,
-            "assists": 3,
-            "age": 21
+            "name": "Marcus Rashford",
+            "position": "Forward",
+            "value": 75000000,
+            "country": "GB",
+            "goals": 15,
+            "assists": 10,
+            "age": 26,
+            "current_club": self.club.id
         }
-        response = self.client.post(self.player_create_url, data)
+        response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Player.objects.count(), 3)
+        self.assertEqual(Player.objects.count(), 3) 
 
-    def test_update_player(self):
-
-        data = {
-            "name": "Updated Player 1",
-            "position": "Goalkeeper",
-            "current_club": self.club.id,
-            "value": "1800000.00",
-            "country": "ES",
-            "goals": 12,
-            "assists": 7,
-            "age": 27
-        }
-        response = self.client.put(self.player_update_url, data)
+    def test_pagination(self):
+        url = reverse('player-list') + '?page=1'
+        response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.player1.refresh_from_db()
-        self.assertEqual(self.player1.name, "Updated Player 1")
-        self.assertEqual(self.player1.position, "Goalkeeper")
-
-    def test_delete_player(self):
-
-        response = self.client.delete(self.player_delete_url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Player.objects.count(), 1)
+        self.assertIn('results', response.data)
+        self.assertEqual(response.data['current_page'], 1)  
+        self.assertEqual(response.data['total_items'], 2) 
